307e7a1b95c1ebf4a96375e58dec12f122305934
t-karasova <91195610+t-karasova@users.noreply.github.com>
2021-10-06 17:32:30 +0200

    Update search_with_filterring.py

--- search_with_filterring.py
+++ search_with_filterring.py
@@ -8,7 +8,6 @@ from google.cloud.retail_v2 import SearchServiceClient, SearchRequest
 
 project_number = "SET HERE VALID PROJECT NUMBER"
 endpoint = "retail.googleapis.com"
-isolation_filter_key = "INTEGRATION_FILTER_KEY"
 title_query = "Nest_Maxi"
 visitor_id = "visitor"
 test_id = ''.join(random.sample(string.ascii_lowercase, 1))
@@ -25,16 +24,12 @@ def get_search_service_client():
 # [END search_client]
 
 
-def build_isolation_filter(test__id: str):
-    return 'attributes.{0}: ANY("{1}")'.format(isolation_filter_key, test__id)
-
-
 # [START search_product_with_filter]
 def search_filtered_products(query: str, _filter: str, page_size=10):
     search_request = SearchRequest()
     search_request.placement = default_search_placement
     search_request.query = query
-    search_request.filter = '{0} AND {1}'.format(build_isolation_filter(test_id), _filter)
+    search_request.filter = _filter
     search_request.page_size = page_size
     search_request.visitor_id = visitor_id
     print("---search_request:---")
@@ -48,7 +43,9 @@ def search_filtered_products(query: str, _filter: str, page_size=10):
 def search():
     setup_catalog.ingest_products(test_id)
 
-    search_response = search_filtered_products(title_query, '(colorFamily: ANY("black"))')
+    filter = '(colorFamily: ANY("black"))'
+    
+    search_response = search_filtered_products(title_query, filter)
     print("---filtered search response---")
     print(search_response)
 
